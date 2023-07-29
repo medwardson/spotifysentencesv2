590a8b62c6c489c0e1e03b557c6ba26c63497f36
Mesha Edwardson <mesha@kikoff.com>
2023-07-28 18:47:46 -0700

    more formatting fixes

--- src/components/search-history.tsx
+++ src/components/search-history.tsx
@@ -1,29 +1,28 @@
 "use client";
 
-import { redirect } from "next/navigation";
-import noBgLogo from "../../public/no-bg-logo.svg";
 import { SearchResult } from "@/types/spotify";
 
+import styles from "./search-history.module.scss";
 interface SearchHistoryProps {
   results: Array<SearchResult>;
 }
 
 export default function SearchHistory({ results }: SearchHistoryProps) {
   return (
-    <div className="text-white">
+    <div className="text-white w-full">
       <div className="text-2xl text-center my-4 font-bold">
         Previous Results
       </div>
       {results.length > 0 ? (
-        <>
+        <div className={styles.grid}>
           {results.map((result, i) => (
-            <div key={i} className="flex flex-row items-center">
-              <div className="text-center text-xl m">
-                {result.title}:{" "}
+            <>
+              <div className="text-left font-bold">{result.title}:</div>
+              <div className={styles.playlistLink}>
                 <a
                   href={result.url}
                   target="_blank"
-                  className={`text-center ${
+                  className={`text-left ${
                     result.url === "No songs found"
                       ? "text-red-400"
                       : "text-green-400"
@@ -32,9 +31,9 @@ export default function SearchHistory({ results }: SearchHistoryProps) {
                   {result.url}
                 </a>
               </div>
-            </div>
+            </>
           ))}
-        </>
+        </div>
       ) : (
         <div className="text-md text-white text-center my-4">
           No previous searches, try one!
